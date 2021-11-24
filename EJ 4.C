5548d8d7504a570f4ee247a16909c22bd73f6476
Gastonviga <87142425+Gastonviga@users.noreply.github.com>
2021-11-24 10:01:53 -0300

    Update EJ 4.C

--- EJ 4.C
+++ EJ 4.C
@@ -1,23 +1,42 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(){
- int T = 0, TL = 0;
- int a[3][12];
- for(int i = 0; i < 3; i++){
- for (int f = 0; f < 12; f++){
- printf("Escriba la recaudacion del colectivo %d de la linea %d: ", f+1, i+1);
- scanf("%d", &a[i][f]);
-}
-}
- for(int i = 0; i < 3; i++){
- TL = 0;
- for (int f = 0; f < 12; f++){
- printf("El colectivo %d de la linea %d es %d\n",f, i, a[i][f]);
- T+= a[i][f];
- TL+= a[i][f];
-}
- printf("La recaudacion total de la linea %d es: %d\n", i, TL);
-}
- printf("la recaudacion total es %d\n", T);
- return 0;
+int main()
+{
+    int matriz[4][13]={0}, sumtt=0, sumln=0, I=0, J=0;
+
+    do{
+        printf("Escriba el numero, la linea y despues la recaudacion del colectivo: ");
+        scanf("\n%d",&J);
+
+        if(J>=1){
+            
+            scanf("\n%d",&I);
+            scanf("\n%d",&matriz[I][J]);
+
+            system("cls");
+            }
+            else{
+                break;
+            }
+    }while(J>=1);
+
+    system("cls");
+
+    for(I=1;I<=3;I++)
+    {
+        sumln=0;
+
+        printf("Linea %d\n", I);
+
+        for (J=1;J<=12;J++)
+        {
+            printf("Recaudacion deL colectivo (%d): %d \n", J, matriz[I][J]);
+
+            sumtt=sumtt+matriz[I][J];
+            sumln=sumln+matriz[I][J];
+        }
+        printf("\nRecaudacion de la linea (%d): %d\n\n", I, sumln);
+    }
+    printf("\nRecaudacion total: %d\n", sumtt);
 }
